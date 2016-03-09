dd793d9a98098c52c7f228c3c7860ec066f09557
Gabor Szabo <szabogabi@gmail.com>
2016-03-09 19:02:07 +0100

    ajax load quiz

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,30 @@
 console.log('\'Allo \'Allo!');
+$(document).foundation();
+
+$(document).ready(function(){
+
+  $('.headmain__videowrap').fitVids();
+
+  $('.movietile__thumb').click(function(e) {
+    e.preventDefault();
+    $('.headmain__videowrap iframe').attr('src', $(this).attr('href'));
+    $('.pagehead').foundation('scrollToLoc', '#headmain');
+  });
+
+
+  $('#thequiz__start').click(function(e) {
+    e.preventDefault();
+    $('#thequiz').removeClass('jollathato');
+
+    setTimeout(function(){
+      $( '#thequiz' ).load( $('#thequiz__start').attr('href') + ' #quiz', function() {
+         $('#thequiz').addClass('jollathato');
+         $('.pagehead').foundation('scrollToLoc', '#thequiz');
+      })}, 200);
+
+  });
+
+
+
+
+});
